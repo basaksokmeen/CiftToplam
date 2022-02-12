116678edcc669f30861948a5c326f3e8fb0d260f
Başak Sökmen <basaksokmeen@gmail.com>
2022-02-12 13:42:38 +0300

    Proje dosyası ekle.

---
+++ CiftToplam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CiftToplam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, top = 0;
+            Console.WriteLine("Sayı giriniz = ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+
+            if (s1 % 2 == 0)
+            {
+                for (int i = s1; i <= 20; i += 2)
+                {
+                    top = top + i;
+                }
+                Console.WriteLine("Toplam = " + top);
+            }
+            else
+            {
+                for (int i = s1; i <= 20; i += 2)
+                {
+                    top = top + i;
+                }
+                Console.WriteLine("Toplam = " + top);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
